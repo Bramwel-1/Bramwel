6a565cadc8b5d2b8ee5de4aa9ee1134668cdf531
Bramwel-1 <151945097+Bramwel-1@users.noreply.github.com>
2023-11-24 21:15:33 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+    $( ".menu-toggle" ).click(function() {
+        $('.menu ul').toggleClass('open');
+    });
+    $( ".search-toggle" ).click(function() {
+        $('.search-bar').toggleClass('open');
+    });
+    $( ".search-close" ).click(function() {
+        $('.search-bar').toggleClass('open');
+    });
+});
